e649159c40cb984bc89aae908ba736356748e418
Tyler Foss <tylerscottfoss@gmail.com>
2017-06-26 23:21:40 -0600

    character display toggle added

--- src/components/CharInfo.js
+++ src/components/CharInfo.js
@@ -7,10 +7,30 @@ class CharInfo extends Component{
       expanded: false,
     }
   }
+  openBox = () => {
+    this.setState({expanded: !this.state.expanded})
+  }
+  closeBox = () => {
+    this.setState({expanded: !this.state.expanded})
+  }
   render(){
-    return(
-      <div></div>
-    )
+    const info = this.props.charInfo;
+    if(!this.state.expanded){
+      return(
+        <p className="btn btn-info" onClick={this.openBox}>Show Info</p>
+      )
+    }else {
+      return(
+        <div className="user-details">
+          <p className="btn btn-danger" onClick={this.closeBox}>Hide Info</p>
+          <ul>
+            <li><h2>Gender: {info.gender}</h2></li>
+            <li><h2>Birth date: {info.birth_year}</h2></li>
+            <li><h2>Hair color: {info.hair_color}</h2></li>
+          </ul>
+        </div>
+      )
+    }
   }
 }
 export default CharInfo;
